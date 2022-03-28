75852e82cea3e5937aabd151b3da6cb6cedc5f11
Lena <lenaschaerer@web.de>
2022-03-28 20:57:25 +0200

    add Article model and navbar changes

--- onlineshop/models.py
+++ onlineshop/models.py
@@ -5,3 +5,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Article (models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=64, decimal_places=2)
+    image_link = models.CharField(max_length=200, default=None, blank=True, null=True)
+
+   
